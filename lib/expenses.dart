7d5da572bfff364a9186f18bc850fca6e0c8fd46
oscar <martososcar.om@gmail.com>
2025-08-12 19:00:57 +0200

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,32 @@
+import 'package:expenses_app/models/expense.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _Expenses();
+  }
+}
+
+class _Expenses extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      amount: 19.99,
+      date: DateTime.now(),
+      title: 'Flutter Course',
+      category: Category.work,
+    ),
+    Expense(
+      amount: 69.99,
+      date: DateTime.now(),
+      title: 'Videogames',
+      category: Category.leisure,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Column(children: [Text('The chart')]));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:expenses_app/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Expenses()));
+}
